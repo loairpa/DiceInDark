d3ce808782a2398a4791a0fab280eaee035240ba
Lovisa Helgadottir <loairpa@gmail.com>
2013-04-21 21:59:36 +0200

    Added new graphics for d4 d6 d10 d12

--- src/com/example/diceindark/Assets.java
+++ src/com/example/diceindark/Assets.java
@@ -36,12 +36,24 @@ public class Assets {
 	    
 	    
 	    public static List<TextureRegion> D4;
+	    public static List<TextureRegion> D6;
+	    public static List<TextureRegion> D8;
+	    public static List<TextureRegion> D10;
+	    public static List<TextureRegion> D12;
 	    public static Animation D4_anim;
+	    public static Animation D6_anim;
+	    public static Animation D8_anim;
+	    public static Animation D10_anim;
+	    public static Animation D12_anim;
 	    public static Sound shakeDie;
 	    public static Sound rollDie;
 	    
 	    public static void load(GLGame game) {
 	    	D4= new ArrayList<TextureRegion>();
+	    	D6= new ArrayList<TextureRegion>();
+	    	D8= new ArrayList<TextureRegion>();
+	    	D10=new ArrayList<TextureRegion>();
+	    	D12= new ArrayList<TextureRegion>();
 	    	mainMenuScreen = new Texture(game, "background.png");
 	    	mainMenuScreenRegion = new TextureRegion(mainMenuScreen, 0, 0, 320, 480);
 	        
@@ -52,8 +64,57 @@ public class Assets {
 	    D4.add(new TextureRegion(items, 320, 0, 320, 480));
 	    D4.add(new TextureRegion(items, 0, 0, 320, 480));
 	    
-	    D4_anim= new Animation(1f, D4.get(0),D4.get(1), D4.get(2),D4.get(3));
+	    D4_anim= new Animation(0.3f, D4.get(0),D4.get(1), D4.get(2),D4.get(3));
 	    
+
+	    D6.add(new TextureRegion(items, 1280+1600, 0, 320, 480));
+	    D6.add(new TextureRegion(items,1280+1280,0,320,480));
+	    D6.add(new TextureRegion(items, 1280+960, 0, 320, 480));
+	    D6.add(new TextureRegion(items, 1280+640, 0, 320, 480));
+	    D6.add(new TextureRegion(items, 1280+320, 0, 320, 480));
+	    D6.add(new TextureRegion(items,1280+0,0,320,480));
+	    
+	    D6_anim= new Animation(0.3f, D6.get(0),D6.get(1), D6.get(2),D6.get(3), D6.get(4),D6.get(5));
+	    
+
+	    D8.add(new TextureRegion(items, 2240, 480, 320, 480));
+	    D8.add(new TextureRegion(items,1920,480,320,480));
+	    D8.add(new TextureRegion(items, 1600, 480, 320, 480));
+	    D8.add(new TextureRegion(items,1280,480,320,480));
+	    D8.add(new TextureRegion(items, 960, 480, 320, 480));
+	    D8.add(new TextureRegion(items, 640, 480, 320, 480));
+	    D8.add(new TextureRegion(items, 320, 480, 320, 480));
+	    D8.add(new TextureRegion(items,0,480,320,480));
+	    
+	    D8_anim= new Animation(0.3f, D8.get(0),D8.get(1), D8.get(2),D8.get(3), D8.get(4),D8.get(5),D8.get(6),D8.get(7));
+	    
+
+	    D10.add(new TextureRegion(items,0,960+480,320,480));
+	    D10.add(new TextureRegion(items, 320, 960+480, 320, 480));
+	    D10.add(new TextureRegion(items, 640, 960+480, 320, 480));
+	    D10.add(new TextureRegion(items, 960, 960+480, 320, 480));	
+	    D10.add(new TextureRegion(items,1280,960+480,320,480));
+	    D10.add(new TextureRegion(items, 1600, 960+480, 320, 480));
+	    D10.add(new TextureRegion(items,1920,960+480,320,480));
+	    D10.add(new TextureRegion(items, 2240, 960+480, 320, 480));
+	    D10.add(new TextureRegion(items,2560,960+480,320,480));
+	    D10.add(new TextureRegion(items, 2880, 960+480, 320, 480));
+	    D10_anim= new Animation(0.3f, D10.get(0),D10.get(1), D10.get(2),D10.get(3), D10.get(4),D10.get(5),D10.get(6),D10.get(7),D10.get(8),D10.get(9));
+	    
+	    D12.add(new TextureRegion(items, 2880, 960, 320, 480));
+	    D12.add(new TextureRegion(items,2560,960,320,480));
+	    D12.add(new TextureRegion(items, 2240, 960, 320, 480));
+	    D12.add(new TextureRegion(items,1920,960,320,480));
+	    D12.add(new TextureRegion(items, 1600, 960, 320, 480));
+	    D12.add(new TextureRegion(items,1280,960,320,480));
+	    D12.add(new TextureRegion(items, 960, 960, 320, 480));
+	    D12.add(new TextureRegion(items, 640, 960, 320, 480));
+	    D12.add(new TextureRegion(items, 320, 960, 320, 480));
+	    D12.add(new TextureRegion(items,0,960,320,480));
+	    D12.add(new TextureRegion(items, 2880, 480, 320, 480));
+	    D12.add(new TextureRegion(items,2560,480,320,480));
+	    
+	    D12_anim= new Animation(0.3f, D12.get(0),D12.get(1), D12.get(2),D12.get(3), D12.get(4),D12.get(5),D12.get(6),D12.get(7),D12.get(8),D12.get(9),D12.get(10),D12.get(11));
 	       rollDie=game.getAudio().newSound("ShakeAndRollDice-SoundBible.com-591494296.mp3");
 	       shakeDie = game.getAudio().newSound("ShakeDice-SoundBible.com-1630587513.mp3");
 	       
--- src/com/example/diceindark/DiceRender.java
+++ src/com/example/diceindark/DiceRender.java
@@ -1,5 +1,7 @@
 package com.example.diceindark;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import javax.microedition.khronos.opengles.GL10;
@@ -20,14 +22,29 @@ public class DiceRender {
 	DiceScreen dice;
 	Camera2D cam;
 	SpriteBatcher batcher;
+	List<List<TextureRegion>> diceTexture;
+	List<Animation> diceAnimation;
 	
-	int direction=0;
 	public DiceRender(GLGraphics glGraphics, SpriteBatcher batcher, DiceScreen dice){
 		this.glGraphics= glGraphics;
 		this.dice=dice;
 		this.cam= new Camera2D(glGraphics, FRUSTUM_WIDTH, FRUSTUM_HEIGHT);
 		this.batcher=batcher;
+		diceTexture = new ArrayList<List<TextureRegion>>();
+		diceTexture.add(Assets.D4);
+		diceTexture.add(Assets.D6);
+		diceTexture.add(Assets.D8);
+		diceTexture.add(Assets.D10);
+		diceTexture.add(Assets.D12);
 
+		diceAnimation = new ArrayList<Animation>();
+		diceAnimation.add(Assets.D4_anim);
+		diceAnimation.add(Assets.D6_anim);
+		diceAnimation.add(Assets.D8_anim);
+		diceAnimation.add(Assets.D10_anim);
+		diceAnimation.add(Assets.D12_anim);
+		
+		
 	}
 
 	public void render(){
@@ -39,67 +56,51 @@ public class DiceRender {
 
 
     public void renderDice() {
-		 TextureRegion keyFrame;
+
         GL10 gl = glGraphics.getGL();
         gl.glEnable(GL10.GL_BLEND);
         gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
         
         batcher.beginBatch(Assets.items);
         Die die = dice.dice.get(dice.currentDie);
-        if(die.sides==4){
-        switch(dice.state){
+        TextureRegion keyFrame;
+        if(dice.currentDie<5){
+ 	   switch(dice.state){
         case DiceScreen.DICE_READY:
-        	direction=0;
-		   if(die.hasResult)
-			   batcher.drawSprite(die.position.x+4, die.position.y+7, FRUSTUM_WIDTH, FRUSTUM_HEIGHT, Assets.D4.get(die.result-1));
-		   else
-			   batcher.drawSprite(die.position.x+4, die.position.y+7,FRUSTUM_WIDTH,  FRUSTUM_HEIGHT, Assets.D4.get(0));
-		   break;
+ 		   if(die.hasResult)
+ 			   batcher.drawSprite(die.position.x+4, die.position.y+7, FRUSTUM_WIDTH, FRUSTUM_HEIGHT, diceTexture.get(dice.currentDie).get(die.result-1));
+ 		   else
+ 			   batcher.drawSprite(die.position.x+4, die.position.y+7,FRUSTUM_WIDTH,  FRUSTUM_HEIGHT, diceTexture.get(dice.currentDie).get(0));
+ 		   break;
         case DiceScreen.DICE_SHAKING:
-			 keyFrame = Assets.D4_anim.getKeyFrames(dice.stateTime, Animation.ANIMATION_LOOPING);		 
-			 batcher.drawSprite(die.position.x+4, die.position.y+FRUSTUM_HEIGHT*die.rand.nextFloat(),FRUSTUM_WIDTH,  FRUSTUM_HEIGHT,keyFrame);
-			 break;
-		 default:
-			 keyFrame = Assets.D4_anim.getKeyFrames(dice.stateTime, Animation.ANIMATION_LOOPING);
-			 batcher.drawSprite(die.position.x+4, die.position.y+7,FRUSTUM_WIDTH,  FRUSTUM_HEIGHT,keyFrame);
-			 break;
-			   
+ 			 keyFrame = diceAnimation.get(dice.currentDie).getKeyFrames(die.rand.nextFloat(), Animation.ANIMATION_LOOPING);		 
+ 			 batcher.drawSprite(die.position.x+4, die.position.y+FRUSTUM_HEIGHT*die.rand.nextFloat(),FRUSTUM_WIDTH,  FRUSTUM_HEIGHT,keyFrame);
+ 			 break;
+ 		 default:
+ 			 keyFrame = diceAnimation.get(dice.currentDie).getKeyFrames(die.rand.nextFloat(), Animation.ANIMATION_LOOPING);
+ 			 batcher.drawSprite(die.position.x+4, die.position.y+7,FRUSTUM_WIDTH,  FRUSTUM_HEIGHT,keyFrame);
+ 			 break;
+ 			   
         }
-        }
-        else
-			   batcher.drawSprite(die.position.x+4, die.position.y+7,FRUSTUM_WIDTH,  FRUSTUM_HEIGHT, Assets.D4.get(0));
-        /*switch(dice.currentDie){
-        case 0:
-        	renderD4();
+        }else
+        	batcher.drawSprite(die.position.x+4, die.position.y+7,FRUSTUM_WIDTH,  FRUSTUM_HEIGHT, Assets.D4.get(0));
+       /* switch(die.sides){
+        case 4:
+        	renderD4(die);
         	break;
-        case 1:
-        	renderD6();
+        case 6:
+        	renderD6(die);
         	break;
-        default: 		
+        default:
+        	batcher.drawSprite(die.position.x+4, die.position.y+7,FRUSTUM_WIDTH,  FRUSTUM_HEIGHT, Assets.D4.get(0));
         	break;
         	
         }*/
-
-
+        
         batcher.endBatch();
         gl.glDisable(GL10.GL_BLEND);
     }
 
 
-   private void renderD4(){
-	   switch(dice.state){
-	   case DiceScreen.DICE_READY:
-		   Die D4 = dice.dice.get(dice.currentDie);
-		   if(D4.hasResult){
-			   batcher.drawSprite(D4.position.x, D4.position.y, FRUSTUM_HEIGHT,FRUSTUM_WIDTH, Assets.D4.get(D4.result-1));
-		   }
-		   else
-			   batcher.drawSprite(D4.position.x, D4.position.y, Die.DIE_HEIGHT, Die.DIE_WIDTH, Assets.D4.get(0));
-	   }
-   }
    
-   private void renderD6(){
-	   
-   }
-
 }
