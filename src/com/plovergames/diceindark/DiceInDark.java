f792efb9f6d06b889144d2a3add2a0e6e45da7a0
Lovisa Helgadottir <loairpa@gmail.com>
2013-07-04 15:51:32 +0200

    Issue #11

--- src/com/plovergames/diceindark/DiceInDark.java
+++ src/com/plovergames/diceindark/DiceInDark.java
@@ -1,19 +1,19 @@
 /*    Dice in the dark. D & D app for the blind and seeing impaired,
-*    Copyright (C) <2013r>  <Lovisa Irpa Helgadottir>
-*
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *    Copyright (C) <2013r>  <Lovisa Irpa Helgadottir>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.plovergames.diceindark;
 
 
@@ -37,15 +37,21 @@ public class DiceInDark extends GLGame {
 	boolean firstTimeCreate = true;
 	@Override
 	public Screen getStartScreen() {
-		   /*Intent installIntent = new Intent();
-           installIntent.setAction(
-               TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-           startActivity(installIntent);*/
+
+		int requestCode = 0;
+		Intent installIntent = new Intent();
+		installIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+		startActivityForResult(installIntent, requestCode);
+		
+		if(requestCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS){
+			installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+			startActivity(installIntent);
+		}
 		// TODO Auto-generated method stub
 		return new MainMenuScreen(this);
 	}
 
-	
+
 	public void onSurfaceCreated(GL10 gl, EGLConfig config){
 		super.onSurfaceCreated(gl, config);
 		if(firstTimeCreate){
@@ -55,15 +61,15 @@ public class DiceInDark extends GLGame {
 		else
 			Assets.reload();
 	}
-	
+
 	@Override
 	public void onPause(){
 		super.onPause();
 
 
 	}
-	
-	
-	
+
+
+
 }
 
